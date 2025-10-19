b91b524740eba16ff3f486996a2e6fd26faacee8
Emma Spear <emmspear@gmail.com>
2025-10-19 17:14:52 +0800

    registered models in admin

--- crowdfunding/fundraisers/admin.py
+++ crowdfunding/fundraisers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import SiteStats, Fundraiser, Pledge
+
+admin.site.register(SiteStats)
+admin.site.register(Fundraiser)
+admin.site.register(Pledge)
--- crowdfunding/users/admin.py
+++ crowdfunding/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# Register your models here.
+from .models import CustomUser, Profile
+
+admin.site.register(CustomUser)
+admin.site.register(Profile)
 
 # Register your models here.
