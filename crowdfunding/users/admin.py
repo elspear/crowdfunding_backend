22763a50bcbc451420dd31f502f7ffdd32c50c88
Emma Spear <emmspear@gmail.com>
2025-11-01 10:34:32 +0800

    remove list filter

--- crowdfunding/users/admin.py
+++ crowdfunding/users/admin.py
@@ -5,11 +5,9 @@ from .models import CustomUser, Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'avatar', 'bio', 'location')
     search_fields = ('user__username', 'user__email', 'avatar', 'location')
-    list_filter = ('location')
     readonly_fields = ()  # or ('avatar',) if you want it read-only
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'role')
     search_fields = ('username', 'email', 'role')
-    list_filter = ('role')
