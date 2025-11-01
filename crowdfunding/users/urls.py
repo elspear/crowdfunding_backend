df640563dcaa7b8fc9b7a5dd5ca5b09e1703aa83
Emma Spear <emmspear@gmail.com>
2025-11-01 11:53:30 +0800

    fix

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.CustomUserList.as_view()),
-    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
     path('users/profiles/<int:user_id>/', views.ProfileDetail.as_view()),
     path('users/check-username/<str:username>/', views.CheckUsernameAvailable.as_view()),
+    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
+    path('users/', views.CustomUserList.as_view()),
 ]
